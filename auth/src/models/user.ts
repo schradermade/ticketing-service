e18381ffdaaab45afdfea59b1a3703f96b274924
Nathan Schrader <natecschrader@gmail.com>
2024-05-09 12:10:06 -0700

    wrap User constructor in fn to incorp typescript

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,5 +1,12 @@
 import mongoose from "mongoose";
 
+// An interface that describes the properties
+// that are required to create a new User
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
 // schema - tell mongoose about all props user will have
 const userSchema = new mongoose.Schema({
   email: {
@@ -15,4 +22,9 @@ const userSchema = new mongoose.Schema({
 // feed schema into mongoose, mongoose will create new model out of it
 const User = mongoose.model('User', userSchema);
 
-export { User };
+// wraps new User constructor in order to tie in typescript
+const buildUser = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+export { User, buildUser };
