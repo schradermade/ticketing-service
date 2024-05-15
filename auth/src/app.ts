bdbc96aff49cb480debcca097dfa8e25c9148b14
Nathan Schrader <natecschrader@gmail.com>
2024-05-15 13:35:48 -0700

    break index.js file into 2 seperate, for testing purposes

---
+++ auth/src/app.ts
@@ -0,0 +1,38 @@
+import express from 'express';
+import 'express-async-errors'
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+
+const app = express();
+// reason for this is traffic is being proxied to our
+// application through Ingress nginx
+app.set('trust proxy', true);
+app.use(json());
+app.use(cookieSession({
+  signed: false,
+  // cookies only shared when req made over https connection
+  // this equality check allows for supertest to receive cookie
+  //   if test environment by setting secure=false
+  secure: process.env.NODE_ENV !== 'test'
+}))
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+// catches any route paths not found and throws error
+app.all('*', async (req, res) => {
+  throw new NotFoundError()
+});
+
+app.use(errorHandler)
+
+export { app};
