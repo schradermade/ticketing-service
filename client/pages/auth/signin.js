2ce7ee1afeec5302a45502a7b839457dbc211106
Nathan Schrader <natecschrader@gmail.com>
2024-05-17 11:22:26 -0700

    implemented authform comp

---
+++ client/pages/auth/signin.js
@@ -0,0 +1,7 @@
+import AuthForm from "../../components/AuthForm";
+
+const SignIn = () => {
+  return <AuthForm title='Sign In' url='/api/users/signin' />
+}
+
+export default SignIn;
--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,47 +1,7 @@
-import { useState } from 'react';
-import axios from 'axios';
-import useRequest from '../../hooks/use-request';
+import AuthForm from "../../components/AuthForm";
 
 const SignUp = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const { doRequest, errors } = useRequest({
-    url: '/api/users/signup', 
-    method: 'post', 
-    body: {email, password}}
-  )
-
-  const onSubmit = async (e) => {
-    e.preventDefault();
-    
-    doRequest();
-  }
-
-  return (
-    <>
-      <form onSubmit={onSubmit}>
-        <h1>Sign Up</h1>
-          <div className='form-group'>
-            <label>Email Address</label>
-            <input 
-              className='form-control' 
-              value={email}
-              onChange={e => setEmail(e.target.value)} />
-          </div>
-          <div className='form-group'>
-            <label>Password</label>
-            <input 
-              type="password" 
-              className='form-control' 
-              value={password} 
-              onChange={e => setPassword(e.target.value)} 
-            /> 
-          </div>
-          {errors}
-          <button className='btn btn-primary'>Sign Up</button>
-      </form>
-    </>
-  )
+  return <AuthForm title='Sign Up' url='/api/users/signup' />
 }
 
 export default SignUp;
