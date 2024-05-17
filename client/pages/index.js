4de0537ea98e5ab7a6fb5b483eafea6d296f5f95
Nathan Schrader <natecschrader@gmail.com>
2024-05-17 11:23:36 -0700

    implement build client fn in getInitialProps

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,16 @@
-const MainPage = () => {
-  return (
-    <>
-      <h1>MAIN PAGE</h1>
-    </>
-  )
+import buildClient from "../api/build-client";
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+ 
+  return currentUser ? <h1>You are signed in</h1> : <h1>You are NOT signed in</h1>
+};
+
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context)
+  const { data } = await client.get('/api/users/currentuser')
+  
+  return data;
 }
 
-export default MainPage;
+export default LandingPage;
