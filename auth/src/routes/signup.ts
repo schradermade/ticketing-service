d31d77676086b0e848dd8fcbbf4f6e04a275a74d
Nathan Schrader <natecschrader@gmail.com>
2024-05-20 12:17:45 -0700

    moved reusable code into common npm pck

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,10 +1,9 @@
 import express, { Request, Response} from 'express';
 import { body } from 'express-validator';
 import jwt from 'jsonwebtoken'
-import { validateRequest } from '../middlewares/validate-request';
+import { validateRequest, BadRequestError } from '@tickets-market/common';
 
 import { User } from '../models/user';
-import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,11 +2,6 @@ import express from 'express';
 import 'express-async-errors'
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
 import { errorHandler, NotFoundError } from '@tickets-market/common';
 
 const app = express();
@@ -21,11 +16,6 @@ app.use(cookieSession({
   //   if test environment by setting secure=false
   secure: process.env.NODE_ENV !== 'test'
 }))
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
 
 // catches any route paths not found and throws error
 app.all('*', async (req, res) => {
