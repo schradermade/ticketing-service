a0d79711fe2da2f21d043d50dd325860230dc5bd
Nathan Schrader <natecschrader@gmail.com>
2024-05-21 10:00:53 -0700

    create fake JWT for auth in testing

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -2,10 +2,11 @@ import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import { app } from '../app';
 import request from 'supertest';
+import jwt from 'jsonwebtoken';
 
-    declare global {
-      var getAuthCookie: () => Promise<string[]>;
-    }
+declare global {
+  var getAuthCookie: () => string[];
+}
 
 let mongo: any;
 
@@ -33,18 +34,20 @@ afterAll(async () => {
   await mongoose.connection.close();
 })
 
-global.getAuthCookie = async () => {
-  const email = 'test@test.com'
-  const password = 'password';
-
-  const response = await request(app)
-    .post('/api/users/signup')
-    .send({
-      email,
-      password
-    })
-    .expect(201);
-
-  const cookie = response.get('Set-Cookie');
-  return cookie || [];
+global.getAuthCookie = () => {
+  // build a JWT payload. { id, email }
+  const payload = {
+    id: '12342923',
+    email: 'test@test.com'
+  }
+  // Create the JWT
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+  // Build session object { jwt: MY_JWT }
+  const session = { jwt: token };
+  // Turn that session into JSON
+  const sessionJSON = JSON.stringify(session);
+  // Take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString('base64');
+  // return a string that is the cookie with the encoded data
+  return [`session=${base64}`];
 }
