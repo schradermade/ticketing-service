12074988cb08549c5d91f733fc3736df7546aa1e
Nathan Schrader <natecschrader@gmail.com>
2024-05-21 10:02:51 -0700

    add mongoose model for Ticket

---
+++ tickets/src/models/ticket.ts
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+
+interface TicketAttrs {
+  title: string;
+  price: number;
+  userId: string;
+}
+
+interface TicketModel extends mongoose.Model<TicketDoc> {
+  build(attrs: TicketAttrs): TicketDoc;
+}
+
+interface TicketDoc extends mongoose.Document{
+  title: string;
+  price: number;
+  userId: string;
+}
+
+const ticketSchema = new mongoose.Schema(
+  {
+    title: {
+      // referring to the global String constructor in JavaScript, capital "S"
+      type: String,
+      required: true
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true
+    }
+  }, {
+    toJSON: {
+      // ret is the object that will be turned into JSON
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      }
+    }
+  }
+)
+
+// simply to have typescript a tell us the attributes
+// to be provided
+ticketSchema.statics.build = (attrs: TicketAttrs) => {
+  return new Ticket(attrs);
+}
+
+const Ticket = mongoose.model<TicketDoc, TicketModel>('Ticket', ticketSchema);
+
+export { Ticket };
