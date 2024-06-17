92d59755cd6ecc7d49f5297e2375e83ae9f0ff71
Nathan Schrader <natecschrader@gmail.com>
2024-06-17 15:49:28 -0700

    setup Orders service by copying tickets service code

---
+++ orders/src/app.ts
@@ -0,0 +1,38 @@
+import express from 'express';
+import 'express-async-errors'
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError, currentUser } from '@tickets-market/common';
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index'
+import { updateTicketRouter } from './routes/update';
+
+const app = express();
+// reason for this is traffic is being proxied to our
+// application through Ingress nginx
+app.set('trust proxy', true);
+app.use(json());
+app.use(cookieSession({
+  signed: false,
+  // cookies only shared when req made over https connection
+  // this equality check allows for supertest to receive cookie
+  //   if test environment by setting secure=false
+  secure: process.env.NODE_ENV !== 'test'
+}))
+app.use(currentUser);
+
+
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
+
+// catches any method/route not found and throws error
+app.all('*', async (req, res) => {
+  throw new NotFoundError()
+});
+
+app.use(errorHandler)
+
+export { app};
