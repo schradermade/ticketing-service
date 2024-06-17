0efa7275a14b7d37253fec8aa7ace3ccceb7db16
Nathan Schrader <natecschrader@gmail.com>
2024-06-17 11:57:19 -0700

    add get method on NatsWrapper class to get client prop

--- tickets/src/nats-wrapper.ts
+++ tickets/src/nats-wrapper.ts
@@ -1,17 +1,28 @@
 import nats, { Stan } from 'node-nats-streaming';
 
+// SINGLETON CLASS
+
 class NatsWrapper {
   private _client?: Stan;
 
+  // getter - defines the client property on the instance
+  get client() {
+    if (!this._client) {
+      throw new Error('Cannot access NATS client before connecting');
+    }
+
+    return this._client;
+  }
+
   connect(clusterId: string, clientId: string, url: string) {
     this._client = nats.connect(clusterId, clientId, { url });
-    
+
     return new Promise<void>((resolve, reject) => {
-      this._client!.on('connect', () => {
+      this.client.on('connect', () => {
         console.log('Connected to NATS!')
         resolve();
       });
-      this._client!.on('error', (err) => {
+      this.client.on('error', (err) => {
         reject(err);
       })
     })
