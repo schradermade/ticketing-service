0c6135d53168131059f62207b28e9a4b34830731
Nathan Schrader <natecschrader@gmail.com>
2024-06-17 11:51:53 -0700

    add env vars to depl file, call in connect function

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -10,10 +10,28 @@ const start = async () => {
   if (!process.env.MONGO_URI) {
     throw new Error('MONGO_URI must be defined');
   }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID must be defined');
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL must be defined');
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID must be defined');
+  }
 
   try {
-    await natsWrapper.connect('ticketing', 'some-random-string', 'http://nats-srv:4222');
-    
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connx closed!');
+        process.exit();
+    })
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
     // after this, any other file in app can
     // import mongoose and it will get pre-connected,
     // pre-initialized version of mongoose
