b70726ca254910d7693407d55f2b4045737d731c
Nathan Schrader <natecschrader@gmail.com>
2024-06-17 15:53:19 -0700

    implement natsWrapper

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -3,6 +3,7 @@ import { requireAuth, validateRequest } from '@tickets-market/common';
 import { body } from 'express-validator';
 import { Ticket } from '../models/ticket';
 import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -26,7 +27,7 @@ router.post(
     userId: req.currentUser!.id
     })
   await ticket.save();
-  await new TicketCreatedPublisher(client).publish({
+  await new TicketCreatedPublisher(natsWrapper.client).publish({
     id: ticket.id,
     title: ticket.title,
     price: ticket.price,
