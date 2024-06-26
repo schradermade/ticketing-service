887480a1905a7f76358129d40cbbdff54a2bfaaa
Nathan Schrader <natecschrader@gmail.com>
2024-06-26 15:07:53 -0700

    add prop: version to TicketDoc interface for DB

--- tickets/src/models/ticket.ts
+++ tickets/src/models/ticket.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { updateIfCurrentPlugin } from "mongoose-update-if-current";
 
 interface TicketAttrs {
   title: string;
@@ -14,6 +15,7 @@ interface TicketDoc extends mongoose.Document{
   title: string;
   price: number;
   userId: string;
+  version: number;
 }
 
 const ticketSchema = new mongoose.Schema(
@@ -41,6 +43,9 @@ const ticketSchema = new mongoose.Schema(
     }
   }
 )
+
+ticketSchema.set('versionKey', 'version')
+ticketSchema.plugin(updateIfCurrentPlugin)
 
 // simply to have typescript a tell us the attributes
 // to be provided
