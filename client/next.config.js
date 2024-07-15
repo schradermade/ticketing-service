087cb7c7e95bd2cf5da64eed58562516a64192ae
Nathan Schrader <natecschrader@gmail.com>
2024-07-15 12:05:40 -0700

    add stripe key to env config

--- client/next.config.js
+++ client/next.config.js
@@ -2,5 +2,8 @@ module.exports = {
   webpackDevMiddleware: config => {
     config.watchOptions.poll = 300;
     return config;
+  },
+  env: {
+    STRIPE_PUBLISH_KEY: process.env.STRIPE_PUBLISH_KEY
   }
 }
